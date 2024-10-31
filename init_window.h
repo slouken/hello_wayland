80e107dba264b088d385f429d6ad317f9c6b288f
Sam Lantinga <slouken@libsdl.org>
2024-10-31 08:18:22 -0700

    Initial changes to support testffmpeg_rpi

--- init_window.h
+++ init_window.h
@@ -10,13 +10,16 @@ typedef struct vid_out_env_s vid_out_env_t;
 
 struct AVFrame;
 struct AVCodecContext;
+struct AVDRMFrameDescriptor;
 
 int vidout_wayland_get_buffer2(struct AVCodecContext *s, struct AVFrame *frame, int flags);
 void vidout_wayland_modeset(struct vid_out_env_s * dpo, int w, int h, AVRational frame_rate);
 int vidout_wayland_display(struct vid_out_env_s * dpo, struct AVFrame * frame);
+const struct AVDRMFrameDescriptor *get_frame_drm_descriptor(struct AVFrame *const frame);
+void add_frame_fence(vid_out_env_t * const ve, struct AVFrame *const frame);
 struct vid_out_env_s * vidout_wayland_new(unsigned int flags);
-
 struct vid_out_env_s * dmabuf_wayland_out_new(unsigned int flags);
+struct vid_out_env_s * vidout_simple_new();
 void vidout_wayland_delete(struct vid_out_env_s * dpo);
 
 void vidout_wayland_runticker(struct vid_out_env_s * dpo, const char * text);
